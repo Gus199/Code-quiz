3787390f3a9896273e650b34f5a415b4ee6290f9
apple <apple@apples-MBP.fios-router.home>
2021-10-02 10:09:13 -0400

    Add initial files

---
+++ main.js
@@ -0,0 +1,47 @@
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var startButton = document.getElementById('start');
+var mainContainer = document.querySelector('.main-container');
+
+
+
+
+function starstartQuiz(){
+    startButton.addEventListener("click", question);
+}
+
+function question(e){
+    mainContainer.innerHTML ="";
+    var h2El = document.createElement('h2');
+    h2El.textContent =" Commonly used data type DO NOT include:"
+    h2El.setAttribute("style"," text-align: center;")
+
+//  its start form here;
+    var arr = ['1.string', '2.booleans', '3.alerts', '4.numbers'];
+    // var cont = document.getElementById('container');
+
+    // create ul element and set the attributes.
+    var ul = document.createElement('ul');
+    ul.setAttribute('style', 'padding: 0; margin: 0;');
+    ul.setAttribute('id', 'theList');
+
+    for (i = 0; i <= arr.length - 1; i++) {
+        var li = document.createElement('button');     // create li element.
+        li.innerHTML = arr[i]; 
+          // assigning text to li using array value.
+          
+        li.setAttribute('style', 'display: block;', 'text-align: center;');    // remove the bullets.
+
+        ul.appendChild(li);     // append li to ul.
+        
+    }
+    mainContainer.appendChild(h2El);
+    mainContainer.appendChild(ul);       // add list to the container.
+ 
+ 
+    event.preventDefault();
+   
+}
+
+   
+starstartQuiz();
